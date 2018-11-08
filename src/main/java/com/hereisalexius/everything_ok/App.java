96a2e4f58da7a69306f2fe6bf570c002c03981cd
Olexij <hereisalexius@gmail.com>
2018-11-09 00:42:41 +0200

    forget to build!

--- src/main/java/com/hereisalexius/everything_ok/App.java
+++ src/main/java/com/hereisalexius/everything_ok/App.java
@@ -8,14 +8,14 @@ public class App
 {
     public static void main( String[] args )
     {
-    	/*
+
     	String f0 = args[0];
     	String f1 = args[1];
-    	*/
-  
+    	
+    	/*	
     	String f0 = "/home/hereisalexius/pages/sample-0-origin.html";
     	String f1 = "/home/hereisalexius/pages/sample-1-evil-gemini.html";
-      	/*	String f2 = "/home/hereisalexius/pages/sample-2-container-and-clone.html";
+      	String f2 = "/home/hereisalexius/pages/sample-2-container-and-clone.html";
     	String f3 = "/home/hereisalexius/pages/sample-3-the-escape.html";
     	String f4 = "/home/hereisalexius/pages/sample-4-the-mash.html";
     	*/
